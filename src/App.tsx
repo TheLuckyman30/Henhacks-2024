84482c863108443ff8a08f4a592095fb0a560b2c
TheLuckyman30 <bnauta0@gmail.com>
2024-03-03 10:01:14 -0500

    Added Second Button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import { DropFile } from "./components/FileDragger";
 import { MakeUppercase } from "./components/MakeUppercase";
+import { MakeLastUpper } from "./components/MakeLastUpper";
 
 function App() {
   const [isDragOver, setisDragOver] = useState<boolean>(false);
@@ -19,6 +20,14 @@ function App() {
           words={words}
           setWords={setWords}
         ></MakeUppercase>
+        <MakeLastUpper
+          isDragOver={isDragOver}
+          setIsDragOver={setisDragOver}
+          file={file}
+          setFile={setFile}
+          words={words}
+          setWords={setWords}
+        ></MakeLastUpper>
         <DropFile
           isDragOver={isDragOver}
           setIsDragOver={setisDragOver}
@@ -29,7 +38,8 @@ function App() {
         ></DropFile>
         <p
           style={{ display: "flex", whiteSpace: "pre-line", fontSize: "20px" }}
-        >{words}
+        >
+          {words}
         </p>
       </div>
     </div>
